f28d2a355db3d7e6ecc3ece0de0d7083c5d56b04
unknown <anna.slapik@slapik8.altoros.corp>
2013-11-05 18:27:07 +0300

    Music store initial project with a few functionality

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>()
+            {
+                new Genre {Name = "Disco"},
+                new Genre {Name = "Jazz"},
+                new Genre {Name = "Rock"}
+            };
+            return View(genres);
+        }
+
+        //
+        // GET: /Browse/
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Details/
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+    }
+}
